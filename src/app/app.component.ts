800cab1dffe8b3db422004c076fa439e3479e4df
MarciaMartins <marciapmartins@gmail.com>
2021-03-11 16:07:31 +0000

    nfFor multiplas fotos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Alura Angular Parte 01';
 
-  photos = [];
+  photos = [
+    {
+      url:'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg', 
+      description:'Leoa'
+    },  
+    {
+      url:'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
+      description:'Leão'
+    }
+  ];
 
 }
